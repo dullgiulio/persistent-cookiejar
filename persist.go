0fa29424e2b0074f3b82b8875dcf65ae9097d5f4
Giulio Iotti <dullgiulio@gmail.com>
2015-09-13 16:10:58 +0300

    retry

--- persist.go
+++ persist.go
@@ -9,16 +9,16 @@ import (
 	"time"
 )
 
-type AtomicFile struct {
+type atomicFile struct {
 	filename string
 	file     *os.File
 	ctime    time.Time
 }
 
-var errAtomicFileRetry = errors.New("original file newer than new contents")
+var erratomicFileRetry = errors.New("original file newer than new contents")
 
 // Write file to temp and atomically move when everything else succeeds.
-func (a *AtomicFile) Create(filename string) (f *os.File, err error) {
+func (a *atomicFile) create(filename string) (f *os.File, err error) {
 	a.filename = filename
 	dir, name := path.Split(filepath.ToSlash(filename))
 	a.file, err = ioutil.TempFile(dir, name)
@@ -28,11 +28,11 @@ func (a *AtomicFile) Create(filename string) (f *os.File, err error) {
 	return a.file, err
 }
 
-func (a *AtomicFile) IsRetry(err error) bool {
-	return err == errAtomicFileRetry
+func (a *atomicFile) isRetry(err error) bool {
+	return err == erratomicFileRetry
 }
 
-func (a *AtomicFile) Cancel() error {
+func (a *atomicFile) cancel() error {
 	if a.file == nil {
 		return nil
 	}
@@ -44,7 +44,7 @@ func (a *AtomicFile) Cancel() error {
 	return err
 }
 
-func (a *AtomicFile) Close() error {
+func (a *atomicFile) close() error {
 	if a.file == nil {
 		return nil
 	}
@@ -56,14 +56,14 @@ func (a *AtomicFile) Close() error {
 	if err == nil || os.IsNotExist(err) {
 		// File was modified after we started writing to the new version.
 		if fi != nil && fi.ModTime().After(a.ctime) {
-			err = errAtomicFileRetry
+			err = erratomicFileRetry
 		} else {
 			err = os.Rename(a.file.Name(), a.filename)
 		}
 	}
 	// Any err should result in full cleanup.
 	if err != nil {
-		a.Cancel()
+		a.cancel()
 	} else {
 		a.file.Close()
 	}
--- serialize.go
+++ serialize.go
@@ -21,21 +21,43 @@ func (j *Jar) Save() error {
 	if j.filename == "" {
 		return errors.New("save called on non-loaded cookie jar")
 	}
-	var af AtomicFile
-	f, err := af.Create(j.filename)
-	if err != nil {
-		return err
+	for {
+		// Create a temporary file
+		var af atomicFile
+		f, err := af.create(j.filename)
+		if err != nil {
+			return err
+		}
+		// Write out to the temporary file
+		err = j.WriteTo(f)
+		if err != nil {
+			// On write error, remove the temp file
+			af.cancel()
+			return err
+		}
+		// Try replacing the original file with our temporary one.
+		// If the file to be replaced is newer, close() fails.
+		err = af.close()
+		// Success.
+		if err == nil {
+			return nil
+		}
+		// We failed, remove the temporary file.
+		af.cancel()
+		// Some error occurred, not related to retrying mechanism.
+		if !af.isRetry(err) {
+			return err
+		}
+		// Load the entries from the file to overwrite.
+		m := make(map[string]map[string]entry)
+		if err := loadJSON(j.filename, m); err != nil {
+			continue
+		}
+		// Merge them on top of ours (they are newer).
+		j.mu.Lock()
+		j.mergeEntries(m)
+		j.mu.Unlock()
 	}
-	err = j.WriteTo(f)
-	if err != nil {
-		af.Cancel()
-		return err
-	}
-	err = af.Close()
-	if af.IsRetry(err) {
-		// TODO: Read again, merge, save.
-	}
-	return err
 }
 
 // Load uses j.ReadFrom to read cookies
@@ -46,7 +68,45 @@ func (j *Jar) Save() error {
 // The path will be stored in the jar and
 // used when j.Save is next called.
 func (j *Jar) Load(path string) error {
+	j.mu.Lock()
+	defer j.mu.Unlock()
+	if err := loadJSON(path, j.entries); err != nil {
+		return err
+	}
 	j.filename = path
+	return nil
+}
+
+// WriteTo writes all the cookies in the jar to w
+// as a JSON object.
+func (j *Jar) WriteTo(w io.Writer) error {
+	// TODO don't store non-persistent cookies.
+	j.mu.Lock()
+	defer j.mu.Unlock()
+	return encodeJSON(w, j.entries)
+}
+
+// ReadFrom reads all the cookies from r
+// and stores them in the Jar.
+func (j *Jar) ReadFrom(r io.Reader) error {
+	// TODO verification and expiry on read.
+	j.mu.Lock()
+	defer j.mu.Unlock()
+	return decodeJSON(r, j.entries)
+}
+
+func (j *Jar) mergeEntries(m map[string]map[string]entry) {
+	for k0 := range m {
+		if _, ok := j.entries[k0]; !ok {
+			j.entries[k0] = make(map[string]entry)
+		}
+		for k1 := range m[k0] {
+			j.entries[k0][k1] = m[k0][k1]
+		}
+	}
+}
+
+func loadJSON(path string, m map[string]map[string]entry) error {
 	f, err := os.Open(path)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -55,31 +115,15 @@ func (j *Jar) Load(path string) error {
 		return err
 	}
 	defer f.Close()
-	return j.ReadFrom(f)
+	return decodeJSON(f, m)
 }
 
-// WriteTo writes all the cookies in the jar to w
-// as a JSON object.
-func (j *Jar) WriteTo(w io.Writer) error {
-	// TODO don't store non-persistent cookies.
+func encodeJSON(w io.Writer, m map[string]map[string]entry) error {
 	encoder := json.NewEncoder(w)
-	j.mu.Lock()
-	defer j.mu.Unlock()
-	if err := encoder.Encode(j.entries); err != nil {
-		return err
-	}
-	return nil
+	return encoder.Encode(m)
 }
 
-// ReadFrom reads all the cookies from r
-// and stores them in the Jar.
-func (j *Jar) ReadFrom(r io.Reader) error {
-	// TODO verification and expiry on read.
+func decodeJSON(r io.Reader, m map[string]map[string]entry) error {
 	decoder := json.NewDecoder(r)
-	j.mu.Lock()
-	defer j.mu.Unlock()
-	if err := decoder.Decode(&j.entries); err != nil {
-		return err
-	}
-	return nil
+	return decoder.Decode(&m)
 }
